e5b7e3432c0ff5c389f88aa655a815e66ea53d4f
aburkard <andrewburkard@gmail.com>
2016-12-05 14:39:50 -0500

    Add Chicago geo bounding box

--- analyze_tweets.py
+++ analyze_tweets.py
@@ -16,7 +16,29 @@ doc = {
 res = es.get(index="twitter", doc_type='tweet', id=796662644161683456)
 print(res['_source'])
 
-res = es.search(index="twitter", body={"query": {"match_all": {}}})
+res = es.search(index="twitter", body={
+    "query": {
+        "bool" : {
+            "must" : {
+                "match_all" : {}
+            },
+            "filter" : {
+                "geo_bounding_box" : {
+                    "coordinates.coordinates" : {
+                        "top_left" : {
+                            "lat" : 42.023135,
+                            "lon" : -87.940101
+                        },
+                        "bottom_right" : {
+                            "lat" : 41.644286,
+                            "lon" : -87.523661
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
 print("Got %d Hits:" % res['hits']['total'])
-#for hit in res['hits']['hits']:
-#    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+for hit in res['hits']['hits']:
+    print(hit["_source"])
